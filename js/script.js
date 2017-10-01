65d7945d718a8d0e4540da7ed3cdfc298638b40f
Michael Li <michael.li1324@gmail.com>
2017-09-30 17:04:09 -0700

    Create Nav Bar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+});
+
+$('.nav-item').hover(function() {
+    $(this).css("background-color", "#2d2d2d");
+}, function() {
+    $(this).css("background-color", "#4B4B4B");
+})
